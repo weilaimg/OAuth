d056974de001bfd328ad8197f022537fd47c092a
weilaimg1314 <weilaimg1314@gmail.com>
2016-07-29 19:45:26 +0800

    查看accesstoken

--- callback.php
+++ callback.php
@@ -1,6 +1,13 @@
 <?php 
+require_once('function.php');
+require_once('Connect2.1/qqConnectAPI.php');
 
-	echo $_GET['code'];
+//请求AccessToken
 
-	
+$oauth = new Oauth();
+$accesstoken = $oauth -> qq_callback();
+
+$debug ($accesstoken);
+
+
  ?>

b2bb2e3e27793298af84f6641d22d85dbf061ef8
weilaimg1314 <weilaimg1314@gmail.com>
2016-07-29 19:58:59 +0800

    调试输出OPENID

--- callback.php
+++ callback.php
@@ -6,8 +6,8 @@ require_once('Connect2.1/qqConnectAPI.php');
 
 $oauth = new Oauth();
 $accesstoken = $oauth -> qq_callback();
-
-debug($accesstoken);
+$openid = $oauth -> get_openid();
+debug($openid);
 
 
  ?>

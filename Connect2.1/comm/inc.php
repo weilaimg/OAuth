b1840cf400fb774751c8c27c3f11e5013066a9f8
weilaimg1314 <weilaimg1314@gmail.com>
2016-07-29 18:34:56 +0800

    引入QQ SDK

---
+++ Connect2.1/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101339552","appkey":"a0cab6c0145c2009ed9a5e1984732447","callback":";http://oauth.weilaimg.cn/callback.php","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
